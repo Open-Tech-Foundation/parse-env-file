33c1ef8bd93cfc770bb17819327a151a396749f5
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-06 01:06:18 +0530

    Add parsing of valid env files

--- __tests__/parse-env-file.test.js
+++ __tests__/parse-env-file.test.js
@@ -1,6 +1,6 @@
 const parseEnvFile = require('../src');
 
-describe('When invalid env file passed', () => {
+describe('When invalid env file argument passed', () => {
   test('the empty argument fails with an error', async () => {
     await expect(parseEnvFile()).rejects.toThrow();
     await expect(parseEnvFile('')).rejects.toThrow('');
@@ -8,5 +8,37 @@ describe('When invalid env file passed', () => {
 
   test('the non existence file argument fails with an error', async () => {
     await expect(parseEnvFile('.env.test')).rejects.toThrow('File not found');
+  });
+});
+
+describe('When valid env file passed', () => {
+  const emptyFile = './__tests__/.env.empty';
+  const invalidFile = './__tests__/.env.vars.invalid';
+  const varsOneFile = './__tests__/.env.vars.one';
+  const varsMultiFile = './__tests__/.env.vars.multi';
+
+  it('returns the empty object', async () => {
+    await expect(parseEnvFile(emptyFile)).resolves.toMatchObject({});
+    await expect(parseEnvFile(invalidFile)).resolves.toMatchObject({});
+  });
+
+  it('returns object with one property', async () => {
+    const output = {
+      ENV: 'val',
+    };
+    const data = await parseEnvFile(varsOneFile);
+    expect(Object.keys(data)).toHaveLength(1);
+    expect(data).toMatchObject(output);
+  });
+
+  it('returns object with multi properties', async () => {
+    const output = {
+      ENV: 'val',
+      ENV2: 'val2',
+      ENV3: 'val3',
+    };
+    const data = await parseEnvFile(varsMultiFile);
+    expect(Object.keys(data)).toHaveLength(3);
+    expect(data).toMatchObject(output);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-const { open } = require('fs');
+const { open, readFile } = require('fs');
 const { promisify } = require('util');
 
 const openFileAsync = promisify(open);
+const readFileAsync = promisify(readFile);
 
 module.exports = async filePath => {
   if (!filePath) {
     throw new Error('Invalid file argument');
   }
 
+  let buffer;
   try {
-    await openFileAsync(filePath, 'r');
+    const fd = await openFileAsync(filePath, 'r');
+    buffer = await readFileAsync(fd, { encoding: 'utf8' });
   } catch (error) {
     throw new Error('File not found');
   }
+
+  const parsedObj = {};
+  const envParseRegex = /^((.+?)[=](.*))$/;
+
+  buffer.split('\n').forEach(line => {
+    const match = envParseRegex.exec(line);
+    if (match) {
+      parsedObj[match[2].trim()] = match[3].trim();
+    }
+  });
+  return parsedObj;
 };
