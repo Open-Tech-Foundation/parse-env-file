f72e60747795ac5ba68b9953d11c5c1617f3217e
Thanga Ganapathy <ganapathy888@gmail.com>
2019-06-06 21:04:38 +0530

    Update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
+import { eslint } from 'rollup-plugin-eslint';
 import filesize from 'rollup-plugin-filesize';
 
 import pkg from './package.json';
 
 export default [
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    {
-        input: 'src/index.js',
-        output: [
-            { file: `dist/${pkg.main}`, format: 'cjs' },
-            { file: `dist/${pkg.module}`, format: 'es' }
-        ],
-        plugins: [filesize()]
-    }
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: 'src/index.js',
+    output: [
+      { file: `dist/${pkg.main}`, format: 'cjs' },
+      { file: `dist/${pkg.module}`, format: 'es' },
+    ],
+    plugins: [filesize(), eslint({ throwOnError: true })],
+    external: ['fs', 'util'],
+  },
 ];
